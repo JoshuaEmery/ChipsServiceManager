5641c3c69743088ac2060ae1f4263550a0fe96a9
BigGazoo <mrjoshemery@gmail.com>
2019-06-27 20:58:55 -0700

    Adding Remaining Enums
    Added ContactMethod Logtype and TicketResult Enum Models

---
+++ CSMWebCore/Models/LogType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSMWebCore.Models
+{
+    public enum LogType
+    {
+        InitialInspection,
+        ContactCustomer,
+        GeneralTroubleshooting,
+        HardwareTroubleshooting,
+        HardwareReplacement,
+        DataBackup,
+        DataRestore,
+        SoftwareInstallation,
+        SoftwareUpdates,
+        OSInstallation,
+        OSUpdate,
+        OfficeInstallation,
+        MalwareScan,
+        MalwareRemoval
+    }
+}
