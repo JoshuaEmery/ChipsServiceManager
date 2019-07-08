b5f35cb50b7af5715a48afb81cf691a247fe2744
BigGazoo <mrjoshemery@gmail.com>
2019-07-08 16:04:33 -0700

    Added logviewmodels
    Log view and edit view model added

--- CSMWebCore/Controllers/LogController.cs
+++ CSMWebCore/Controllers/LogController.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSMWebCore.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSMWebCore.Controllers
 {
+    [Authorize]
     public class LogController : Controller
     {
+        private IDeviceData _devices;
+        private ICustomerData _customers;
+        private ITicketData _tickets;
+        private ILogData _logs;
+
+        public LogController(IDeviceData devices, ICustomerData customers, ITicketData tickets, ILogData logs)
+        {
+            _devices = devices;
+            _customers = customers;
+            _tickets = tickets;
+            _logs = logs;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
         {
             return View();
         }
