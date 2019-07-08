76fc3cdcae8b190899ba95321371f4901857e30b
BigGazoo <mrjoshemery@gmail.com>
2019-07-08 14:53:27 -0700

    Added Ticket Create Action
    Initial Ticket Create Action functioning

--- CSMWebCore/Controllers/TicketController.cs
+++ CSMWebCore/Controllers/TicketController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using CSMWebCore.Entities;
+using CSMWebCore.Models;
 using CSMWebCore.Services;
+using CSMWebCore.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSMWebCore.Controllers
 {
+    [Authorize]
     public class TicketController : Controller
     {
         private IDeviceData _devices;
@@ -25,17 +30,42 @@ namespace CSMWebCore.Controllers
 
         public IActionResult Index()
         {
-            //var userName = User.FindFirst(ClaimTypes.Name).Value;
+            
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create(int deviceId)
+        {
+            var device = _devices.Get(deviceId);
+            var cust = _customers.Get(device.CustomerId);            
+            if(cust == null || device == null)
+            {
+                return NotFound();
+            }
+            TicketEditViewModel model = new TicketEditViewModel
+            {
+                Customer = cust,
+                Device = device
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Create(TicketEditViewModel model)
+        {
+            if(model == null || !ModelState.IsValid)
+            {
+                return View();
+            }
+            _tickets.Add(
+                new Ticket { DeviceId = model.DeviceId,
+                    CheckInUserId = User.FindFirst(ClaimTypes.Name).Value.ToString(),
+                    CheckedIn = DateTime.Now,
+                    NeedsBackup = model.NeedsBackup,
+                    TicketStatus = TicketStatus.New                               
+                });
+            _tickets.Commit();
+            return RedirectToAction("Index");
+        }
 
-            return View();
-        }
-        public IActionResult CreateNew()
-        {
-            return View();
-        }
-        public IActionResult CreateExisting()
-        {
-            return View();
-        }
     }
 }
