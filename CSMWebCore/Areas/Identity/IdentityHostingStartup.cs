1ddcc5a2bc5d5a792df2368008fd2cadd7514f4e
BigGazoo <mrjoshemery@gmail.com>
2019-07-09 15:25:26 -0700

    Scaffolded Register
    Added Register Scaffolding, Requires login to create a new user

---
+++ CSMWebCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CSMWebCore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CSMWebCore.Areas.Identity.IdentityHostingStartup))]
+namespace CSMWebCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
