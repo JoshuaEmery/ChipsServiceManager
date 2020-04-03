113811ac4660bbcc9bd40c3ffd97f1ce6856a965
Emery, Joshua D <mrjoshemery@gmail.com>
2020-04-03 14:36:44 -0700

    Intellisense info needed to be in interface, not implementation.  Intellisense provides information regarding ReportsService methods now.

--- CSMWebCore/Services/ReportsService.cs
+++ CSMWebCore/Services/ReportsService.cs
@@ -32,11 +32,7 @@ namespace CSMWebCore.Services
             _userManager = userManager;
             _servicePrices = servicePrices;
         }
-        /// <summary>
-        /// Gets count of active tickets, TicketStatus optional parameter
-        /// </summary>
-        /// <param name="status"></param>
-        /// <returns></returns>
+
         public int TotalActiveTickets(TicketStatus? status = null)
         {
 
@@ -45,11 +41,7 @@ namespace CSMWebCore.Services
             else
                 return _tickets.GetByStatus(status.Value).Count();
         }
-        /// <summary>
-        /// Gets count of completed tickets, TimeSpan optional parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int TotalCompletedTickets(TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -57,21 +49,12 @@ namespace CSMWebCore.Services
             else
                 return _tickets.GetCompletedTickets(span.Value).Count();
         }
-        /// <summary>
-        /// Gets count of completed tickets between dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int TotalCompletedTickets(DateTime startDate, DateTime endDate)
         {
             return _tickets.GetCompletedTickets(startDate, endDate).Count();
         }
-        /// <summary>
-        /// Returns count of Checked in tickets, TimeSpan optional parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int TotalCheckedInTickets(TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -79,21 +62,12 @@ namespace CSMWebCore.Services
             else
                 return _tickets.GetCheckedInTickets(span.Value).Count();
         }
-        /// <summary>
-        /// Returns count of Checked in tickets between two dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int TotalCheckedInTickets(DateTime startDate, DateTime endDate)
         {
             return _tickets.GetCheckedInTickets(startDate, endDate).Count();
         }
-        /// <summary>
-        /// Returns count of Checked out tickets, timespan optional parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int TotalCheckedOutTickets(TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -101,22 +75,12 @@ namespace CSMWebCore.Services
             else
                 return _tickets.GetCheckedOutTickets(span.Value).Count();
         }
-        /// <summary>
-        /// Returns count of checked out tickets between two dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int TotalCheckedOutTickets(DateTime startDate, DateTime endDate)
         {
             return _tickets.GetCheckedOutTickets(startDate, endDate).Count();
         }
-        /// <summary>
-        /// Gets count of contact logs by a given user, timespan optional parameter
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int GetContactLogsByUser(string userName, TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -124,23 +88,12 @@ namespace CSMWebCore.Services
             else
                 return _logs.GetContactLogsByUser(userName, span.Value).Count();
         }
-        /// <summary>
-        /// Gets count of contact logs by a given user between two dates
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int GetContactLogsByUser(string userName, DateTime startDate, DateTime endDate)
         {
             return _logs.GetContactLogsByUser(userName, startDate, endDate).Count();
         }
-        /// <summary>
-        /// Gets count of service logs by a given user, timespan optional parameter
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int GetServiceLogsByUser(string userName, TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -148,22 +101,12 @@ namespace CSMWebCore.Services
             else
                 return _logs.GetServiceLogsByUser(userName, span.Value).Count();
         }
-        /// <summary>
-        /// Gets count of service logs by a given user between two dates
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int GetServiceLogsByUser(string userName, DateTime startDate, DateTime endDate)
         {
             return _logs.GetServiceLogsByUser(userName, startDate, endDate).Count();
         }
-        /// <summary>
-        /// Gets count of consultations, timespan optional parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int GetConsultations(TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -171,22 +114,12 @@ namespace CSMWebCore.Services
             else
                 return _consultations.GetConsultations(span.Value).Count();
         }
-        /// <summary>
-        /// Gets count of consultations between two dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int GetConsultations(DateTime startDate, DateTime endDate)
         {
             return _consultations.GetConsultations(startDate, endDate).Count();
         }
-        /// <summary>
-        /// Gets count of consultations by a given user, timespan optional parameter
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public int GetConsultationsLogsByUser(string userName, TimeSpan? span = null)
         {
             if (!span.HasValue)
@@ -194,41 +127,22 @@ namespace CSMWebCore.Services
             else
                 return _consultations.GetConsultationsByUser(userName, span.Value).Count();
         }
-        /// <summary>
-        /// Gets count of consultations by user between two dates
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public int GetConsultationsLogsByUser(string userName, DateTime startDate, DateTime endDate)
         {
             return _consultations.GetConsultationsByUser(userName, startDate, endDate).Count();
         }
-        /// <summary>
-        /// Generates a TicketProgressReport for the given ticketId
-        /// </summary>
-        /// <param name="ticketId"></param>
-        /// <returns></returns>
-        //-----------Ticket Progress Report
+
         public TicketProgressReport PrintProgressReport(int ticketId)
         {
             return _ticketsHistory.GetTicketProgressReport(_tickets.Get(ticketId));
         }
-        /// <summary>
-        /// Gets total amount of savings by ticketId
-        /// </summary>
-        /// <param name="ticketId"></param>
-        /// <returns></returns>
+
         public decimal GetSavingsByTicket(int ticketId)
         {
             return _servicePrices.GetTotalPrice(_logs.GetDistinctLogTypesByTicketId(ticketId));            
         }
-        /// <summary>
-        /// Gets total savings for all tickets.  Optional timespan parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public decimal GetTicketSavingsOverTimePeriod(TimeSpan? span = null)
         {
             IEnumerable<Ticket> tickets;
@@ -247,12 +161,7 @@ namespace CSMWebCore.Services
             }
             return total;
         }
-        /// <summary>
-        /// Gets the total savings for all tickets between two dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public decimal GetTicketSavingsOverTimePeriod(DateTime startDate, DateTime endDate)
         {
             IEnumerable<Ticket> tickets = _tickets.GetCheckedInTickets(startDate, endDate);
@@ -263,11 +172,7 @@ namespace CSMWebCore.Services
             }
             return total;
         }
-        /// <summary>
-        /// Gets the total savings for all consultations.  Optional timespan parameter
-        /// </summary>
-        /// <param name="span"></param>
-        /// <returns></returns>
+
         public decimal GetConsultSavingsOverTimePeriod(TimeSpan? span = null)
         {
             IEnumerable<Consultation> consults;
@@ -283,12 +188,7 @@ namespace CSMWebCore.Services
             total += _servicePrices.GetPriceOfServiceType(LogType.Diagnostic) * consults.Count();
             return total;
         }
-        /// <summary>
-        /// Gets the total consultation savings between two dates
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="endDate"></param>
-        /// <returns></returns>
+
         public decimal GetConsultSavingsOverTimePeriod(DateTime startDate, DateTime endDate)
         {
             IEnumerable<Consultation> consults = _consultations.GetConsultations(startDate, endDate);
